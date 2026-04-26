55772bfbc562d48d8fcb53a00f577454721053dc
self-reliantkid <syfolikumah@gmail.com>
2026-04-26 03:33:36 +0000

    feat: updates to task_system.py, closes #6

--- task_system.py
+++ task_system.py
@@ -1,4 +1,4 @@
-from utils import get_current_date, clear_screen, gen_id
+from utils import get_current_date, gen_id
 
 
 date = get_current_date()
@@ -18,7 +18,7 @@ def add_task(task, priority, tasks_list):
     task_id = int(last_id) + 1
     task_id = gen_id(task_id)
 
-    task_details = [task, priority, "uncomplete", date, task_id]
+    task_details = [task, priority, "uncompleted", date, task_id]
 
     tasks_list[date][task_id] = task_details
 
@@ -29,8 +29,40 @@ def add_task(task, priority, tasks_list):
 def view_tasks(tasks_list):
     print(f"Tasks for today, {date}")
 
-    for task in tasks_list[date]:
-        print(f"\nTask ID: {tasks_list[date][task][-1]}")
-        print(f"Task: {tasks_list[date][task][0]}")
-        print(f"Priority: {tasks_list[date][task][1]}")
-        print(f"Status: {tasks_list[date][task][2]}")
+    if not tasks_list[date]:
+        print("No available tasks to display!")
+
+    else:    
+        for task in tasks_list[date]:
+            print(f"\nTask ID: {tasks_list[date][task][-1]}")
+            print(f"Task: {tasks_list[date][task][0]}")
+            print(f"Priority: {tasks_list[date][task][1]}")
+            print(f"Status: {tasks_list[date][task][2]}")
+
+        input("\nPRESS ENTER TO CONTINUE...")
+
+
+def mark_complete(id, tasks_list):
+    if id not in tasks_list[date]:
+        print("Task ID not found!")
+    else:
+        tasks_list[date][id][2] = "completed"
+        return tasks_list
+
+
+
+def change_priority(id, priority, tasks_list):
+    if id not in tasks_list[date]:
+        print("Task ID not found!")
+    else:
+        tasks_list[date][id][1] = priority
+        return tasks_list
+
+
+
+def delete_task(id, tasks_list):
+    if id not in tasks_list[date]:
+        print("Task ID not found!")
+    else:
+        tasks_list[date].pop(id, None)
+        return tasks_list
